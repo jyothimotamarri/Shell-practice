f5de4d3a7df004e8e5cb55d42503a944fbb30d23
Jyothi <jyothisuryaraj@gmail.com>
2025-05-15 16:12:20 -0400

    made changes

---
+++ 03.variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1= Agastya 
+Person2= Alex
+
+echo "$person1: hey $person2, how are you?"
+echo "$person2: I am doing well, thank you, how are you?"
+echo "$person1: I am also doing well, what;s up?"
+echo "$person2: looking forward for the summer!"
