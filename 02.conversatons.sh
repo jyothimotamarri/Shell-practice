9244e28429e3f02086609692101d796d230701ac
Jyothi <jyothisuryaraj@gmail.com>
2025-05-15 15:57:47 -0400

    made changes

---
+++ 02.conversatons.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Anu: hey Jyothi, how are you?"
+echo "Jyothi: I am doing well, thank you, how are you?"
+echo "Anu: I am also doing well, what;s up?"
+echo "Jyothi: looking forward for the summer!"
