a1c5801c16d05f643b5ac30de79516e2bb59ddc1
Jyothi <jyothisuryaraj@gmail.com>
2025-05-15 16:26:14 -0400

    made changes

--- 02.conversations.sh
+++ 02.conversations.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+Person1:Agastya
+Person2:Alex
+
 echo "Anu: hey Jyothi, how are you?"
 echo "Jyothi: I am doing well, thank you, how are you?"
 echo "Anu: I am also doing well, what;s up?"
